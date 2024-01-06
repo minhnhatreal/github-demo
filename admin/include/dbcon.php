a89b4472d0f596e69bae16117c8dfc397bf55f48
NGUYEN NHAT MINH <ngocminhnb2002@gmail.com>
2024-01-06 21:03:18 +0700

    add all file

---
+++ admin/include/dbcon.php
@@ -0,0 +1,8 @@
+<?php 
+ $host      = "localhost";
+ $username  = "user2";
+ $passwrod  = "Abc123456..";
+ $dbName    = "tmdt-shop";
+
+  $con = mysqli_connect($host,$username,$passwrod,$dbName);
+   ?>
